b056aab362d993daa9e00da8e9e1ba4d7e58e4ae
Werelaxe <integrex@yandex.ru>
2016-12-09 06:15:10 +0500

    add argparse

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import gzip
 import os
-
+import sys
 from man_parser import ManParser
+import parsing_arguments
+
 
 if __name__ == '__main__':
+    parser = parsing_arguments.get_parser()
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(0)
+    args = vars(parser.parse_args(sys.argv[1:]))
+    print(args)
     os.remove("page.html")
     with open("page.html", 'a') as page:
         with gzip.open('yes.1.gz') as file:
--- parsing_arguments.py
+++ parsing_arguments.py
@@ -1,4 +1,14 @@
 import argparse
 
 
-def parse_arguments():
+def get_parser():
+    parser = argparse.ArgumentParser(description='man to html')
+
+    parser.add_argument('-i', action='store',
+                        dest='input', required=True,
+                        help='input man file')
+
+    parser.add_argument('-o', action='store',
+                        dest='output',
+                        help='output man file', default='page.html')
+    return parser
